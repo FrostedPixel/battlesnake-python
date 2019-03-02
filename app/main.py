25a954f8134cbc841b9409d3c96373ee1b03993c
jwoolfrey <joseph.woolfrey@gmail.com>
2019-03-02 12:43:47 -0800

    refactor of cBoard (cPlayfield) class

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import os
 from Queue import PriorityQueue
 
 # Constants
-cell_value = {
-        'wall':-1,
+cellValue = {
+        'obst':-1,
         'empty':0,
         'food':10,
     }
@@ -15,36 +15,55 @@ directions = {
      }
 
 # Classes
-class cPlayfield():
+class cBoard():
     _fields = {}
     def __init__(self, w, h):
         self.width = w
         self.height = h
 
-        self._fields['obstacles'] = [[cell_value['empty'] for y in range(h)] for x in range(w)]
-        self._fields['movecosts'] = [[1 for y in range(h)] for x in range(w)]
+        self._fields['obstacles']   = [[cellValue['empty'] for y in range(h)] for x in range(w)]
+        self._fields['food']        = [[cellValue['empty'] for y in range(h)] for x in range(w)]
+        self._fields['movecosts']   = [[1 for y in range(h)] for x in range(w)]
 
     def __getitem__(self, id):
         return self._fields[id]
 
     def inBounds(self, pos):
-        return ((pos['x'] >= 0 and pos['x'] < self.width) \
-                and (pos['y'] >= 0 and pos['y'] < self.height))
+        return ((pos[0] >= 0 and pos[0] < self.width) \
+                and (pos[1] >= 0 and pos[1] < self.height))
+
+    def openNeighbours(self, pos):
+        neighbours = []
+        for dr in directions['ortho']:
+            xpos,ypos = pos[0] + dr[0],pos[1] + dr[1]
+            if self.inBounds((xpos,ypos)):
+                if self._fields['obstacles'][xpos][ypos] != cellValue['obst']:
+                    neighbours.append((xpos,ypos))
+        return neighbours
+
+    def findDistance(self, apos, bpos):
+        return (abs(apos[0] - bpos[0]) + abs(apos[1] - bpos[1]))
+
+    def addObstacles(self, obst):
+        for ob in obst:
+            if self.inBounds(ob):
+                self._fields['obstacles'][ob[0]][ob[1]] = cellValue['obst']
+
+    def addFoods(self, food):
+        for fd in food:
+            if self.inBounds(fd):
+                self._fields['food'][fd[0]][fd[1]] = cellValue['food']
 
 class cSnake():
     _map = {}
     def __init__(self, s):
-        self._map['id'] = s['id']
+        self._map['id']     = s['id']
         self._map['health'] = s['health']
         self._map['length'] = len(s['body'])
-        self._map['name'] = s['name']
-        self._map['body'] = s['body']
+        self._map['body']   = [(p['x'],p['y']) for p in s['body']]
 
-        self._map['head'] = self._map['body'][0]
-        self._map['tail'] = self._map['body'][-1]
-        self._map['starving'] = False
-        if self._map['health'] <= behaviour_trigger['starve']:
-            self._map['starving'] = True
+        self._map['head']   = self._map['body'][0]
+        self._map['tail']   = self._map['body'][-1]
 
     def __getitem__(self, key):
         return self._map[key]
@@ -75,7 +94,7 @@ def findShortestPath(playfield, start, target):
         for n in neighbors:
             if (not playfield.inBounds(n)):
                 continue
-            if (playfield['obstacles'][n[0]][n[1]] == cell_value['wall']):
+            if (playfield['obstacles'][n[0]][n[1]] == cellValue['obst']):
                 continue
             newCost = totalCost[currCell] + playfield['movecosts'][n[0]][n[1]]
             if ((n not in totalCost) or (newCost < totalCost[n])):
@@ -106,7 +125,7 @@ def start():
 @bottle.post('/end')
 def end():
     return "ack"
-
+    
 @bottle.post('/ping')
 def ping():
     return "pong"
@@ -118,7 +137,7 @@ def move():
     #movementOptions = {(0,-1):'up', (0,1):'down', (-1,0):'left', (1,0):'right'}
     nextMove = 'down'
 
-    playfield = cPlayfield(data['board']['width'],data['board']['height'])
+    gameBoard = cBoard(data['board']['width'],data['board']['height'])
     ourSnake = cSnake(data['you'])
 
     return {
