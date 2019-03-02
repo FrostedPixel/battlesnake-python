1b295a8d6aa33e6247a596518cc223d1dc533a6f
jwoolfrey <joseph.woolfrey@gmail.com>
2019-03-02 15:56:34 -0800

    I blame Robert

--- app/main.py
+++ app/main.py
@@ -154,7 +154,8 @@ def move():
     preyList = []
     for sk in data['board']['snakes']:
         if len(sk['body']) < ourSnake['length']:
-            preyList.append((sk['body'][0]['x'],sk['body'][0]['y']))
+            #preyList.append((sk['body'][0]['x'],sk['body'][0]['y']))
+            preyList = preyList + gameBoard.openNeighbours((sk['body'][0]['x'],sk['body'][0]['y']))
         for seg in sk['body']:
             gameBoard.addObstacles([(seg['x'],seg['y'])])
 
